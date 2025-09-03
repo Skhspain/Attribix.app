3c4470efb81224c8f465162e2d6f0e9010be3687
Skhspain <stiankhenriksen@gmail.com>
2025-09-03 23:52:07 +0200

    build: remix vite plugin + ~ alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
-import { vitePlugin as remix } from "@remix-run/dev";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from "@remix-run/dev"; // Remix v2 plugin
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()]
+  plugins: [remix()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "app"), // lets you import "~/shopify.server"
+    },
+  },
 });
